d196d2163ffc0767acea979c633de0524cf99f8c
Zachary Milke <milke.zachary.00@gmail.com>
2021-03-16 18:03:42 -0400

    Beautifies the testing code

--- Python/test_calc_manhattan_distance.py
+++ Python/test_calc_manhattan_distance.py
@@ -6,7 +6,7 @@ class TestManhattanCalc(unittest.TestCase):
     def test_manhattan_calc(self):
         origin = MD.Point(0, 0)  # one of the points to be measured from
         
-        # -------------------- begin creating points --------------------
+        # -------------------- begin creating points -------------------- #
         # creating an imaginary 10x10 square's corners
         point_1 = MD.Point(10, 10)
         point_2 = MD.Point(-10, 10)
@@ -25,10 +25,10 @@ class TestManhattanCalc(unittest.TestCase):
         point_11 = MD.Point(47, 100)
         point_12 = MD.Point(-3, 2)
         point_13 = MD.Point(3, -2)
-        # -------------------- end creating points --------------------
+        # ----------------------- end creating points ----------------------- #
 
 
-        # -------------------- begin testing of points --------------------
+        # -------------------- begin testing of points -------------------- #
         self.assertEqual((MD.calc_manhattan_distance(origin, point_1)), 20)
         self.assertEqual((MD.calc_manhattan_distance(origin, point_2)), 20)
         self.assertEqual((MD.calc_manhattan_distance(origin, point_3)), 20)
@@ -51,7 +51,7 @@ class TestManhattanCalc(unittest.TestCase):
         self.assertEqual((MD.calc_manhattan_distance(point_3, point_6)), 15)
         self.assertEqual((MD.calc_manhattan_distance(point_1, origin)), 20)
         self.assertEqual((MD.calc_manhattan_distance(point_8, point_11)), 152)
-        # ----------------------- end testing of points -----------------------
+        # ----------------------- end testing of points ----------------------- #
 
 
 if __name__ == "__main__":
