6b02e29b356a97bb14aefb06c0523827c9a8de79
Zachary Milke <milke.zachary.00@gmail.com>
2021-03-16 18:04:21 -0400

    Adds debuggin features for x and y distance

--- Python/ManhattanDistance.py
+++ Python/ManhattanDistance.py
@@ -7,14 +7,19 @@ class Point:
         return "point: ({} , {})".format(self.x, self.y)
 
 
-def calc_manhattan_distance(first_point, second_point):
-    manhattan_distance = (abs(first_point.x - second_point.x) + abs(first_point.y - second_point.y))
+def calc_manhattan_distance(point_a, point_b):
+    manhattan_distance = (abs(point_a.x - point_b.x) + abs(point_a.y - point_b.y))
     
+    # Three lines below used for debugging total distance
+    #man_dist_x = abs(point_a.x - point_b.x)
+    #man_dist_y = abs(point_a.y - point_b.y)
+    #print("X distance: {}\nY distance: {}".format(man_dist_x, man_dist_y))
+
     # following line is used for testing purposes only
     #return manhattan_distance
 
     # following line is used for final implementation
-    return "Manhattan distance from {} to {} is {}".format(first_point, second_point, manhattan_distance)
+    return "Manhattan distance from {} to {} is {} units.".format(point_a, point_b, manhattan_distance)
 
 
 if __name__ == "__main__":
@@ -22,10 +27,7 @@ if __name__ == "__main__":
         "\n( X , Y ) = 1 2\t\t will produce the point (1, 2) \n( X, Y ) = -45 23\t will produce the point"
         "(-45, 23) and so on...\tCTRL + C to exit.\n")
 
-    testing_chars = []
-
-    # only pairs of integers separated by a space are allowed
-    flag = True
+    flag = True  # Only integers separated by a space as we are inputting coordinates of points
     while flag:
         try:
             x_point_a, y_point_a = input("Point A - ( X , Y ): ").split()
@@ -38,13 +40,14 @@ if __name__ == "__main__":
             if pos_x_point_a.isdigit() and pos_y_point_a.isdigit() and pos_x_point_b.isdigit() and pos_y_point_b.isdigit():
                 flag = False
             else:
-                print("Please only enter integers. Try again.")
+                print("Please enter only integers as we are inputting coordinates. Try again.")
         except KeyboardInterrupt:  # CTRL + C
             print("\nYou have chosen to quit the program, press enter to exit.")
             exit(0)
         except ValueError:         # only one value was entered
             print("Please enter two integers separated by a space. Try again.")
 
+    # creating the two points to be used for calculating
     point_a = Point(int(x_point_a), int(y_point_a))
     point_b = Point(int(x_point_b), int(y_point_b))
 
